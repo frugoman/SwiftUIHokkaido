384745d06e76c39b77ea6327c91fd946b5c02d76
Nicolas Frugoni <nfrugoni19@gmail.com>
2025-04-17 16:21:43 +0200

    Initial Commit

---
+++ SwiftUIHokkaido/SwiftUIHokkaidoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIHokkaidoApp.swift
+//  SwiftUIHokkaido
+//
+//  Created by Nicolas Frugoni on 17/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIHokkaidoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftUIHokkaidoTests/SwiftUIHokkaidoTests.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIHokkaidoTests.swift
+//  SwiftUIHokkaidoTests
+//
+//  Created by Nicolas Frugoni on 17/04/2025.
+//
+
+import Testing
+@testable import SwiftUIHokkaido
+
+struct SwiftUIHokkaidoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
